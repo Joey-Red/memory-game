0aa0f6ba5467aaffd1ad1fcfaa053961ab8d6be4
Joey Dalrymple <xjoey96@gmail.com>
2022-03-08 15:22:33 -0700

    minor progress

---
+++ memory-game/README.md
@@ -0,0 +1,2 @@
+#memory-game
+fun memory game, click the cards - but dont click the same one twice!
---
+++ memory-game/src/components/scoreboard.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+const Scoreboard = props => {
+  const [score, setScore] = useState(0);
+  const [cardArray, setCardArray] = useState([]);
+  useEffect(()=>{
+    if (cardClicked === newCard){
+      console.log("ss")
+    }
+  })
+}
+export default Scoreboard
