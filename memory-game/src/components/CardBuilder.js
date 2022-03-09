4d2ae5ea21688e06a16bd6310bf5c51dda40248c
Joey Dalrymple <xjoey96@gmail.com>
2022-03-09 03:00:52 -0700

    created card builder func

---
+++ memory-game/src/components/CardBuilder.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+const CardBuilder = props => {
+  let Buck = {
+    id: 1,
+    name: 'Brick',
+    img: "./img/buck.jpg"
+  }
+  let Cat = {
+    id: 2,
+    name: 'Cindy',
+    img: "./img/cat.jpg"
+  }
+  let Chameleon = {
+    id: 3,
+    name: 'Carter',
+    img: "./img/chameleon.jpg"
+  }
+  let Dog = {
+    id: 4,
+    name: 'Dorothy',
+    img: "./img/dog.jpg"
+  }
+  let Duck = {
+    id: 5,
+    name: 'Danica',
+    img: "./img/duck.jpg"
+  }
+  let Fox = {
+    id: 6,
+    name: 'Frank',
+    img: "./img/fox.jpg"
+  }
+  let Giraffe = {
+    id: 7,
+    name: 'Georgie',
+    img: "./img/giraffe.jpg"
+  }
+  let Horse = {
+    id: 8,
+    name: 'Hayley',
+    img: "./img/horse.jpg"
+  }
+  let Parrot = {
+    id: 9,
+    name: 'Patsy',
+    img: "./img/parrot.jpg"
+  }
+  let Squid = {
+    id: 10,
+    name: 'Seth',
+    img: "./img/squid.jpg"
+  }
+  let Squirrel = {
+    id: 11,
+    name: 'Sandy',
+    img: "./img/squirrel.jpg"
+  }
+  let Tiger = {
+    id: 12,
+    name: 'Tempie',
+    img: "./img/tiger.jpg"
+  }
+}
