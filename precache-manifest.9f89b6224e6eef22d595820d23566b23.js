7567d02780ef509bdefb5064c6296e453ffe380f
Joey <xjoey96@gmail.com>
2022-08-07 09:02:24 -0500

    Updates

---
+++ precache-manifest.9f89b6224e6eef22d595820d23566b23.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a19e9a0ddd20b2c83b68",
+    "url": "/memory-game/static/css/main.7623fb8c.chunk.css"
+  },
+  {
+    "revision": "a19e9a0ddd20b2c83b68",
+    "url": "/memory-game/static/js/main.0ba60cb4.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "fda2b1e0fb6e1b38d170",
+    "url": "/memory-game/static/js/2.b126469f.chunk.js"
+  },
+  {
+    "revision": "302dcb3405a69fe6cd8ff58c0edca91f",
+    "url": "/memory-game/static/media/buck.302dcb34.jpg"
+  },
+  {
+    "revision": "65b5a38582387ceaec2637b13cbe438d",
+    "url": "/memory-game/static/media/cat.65b5a385.jpg"
+  },
+  {
+    "revision": "5fe400f9877f4b41e6daf75347ea9df1",
+    "url": "/memory-game/static/media/chameleon.5fe400f9.jpg"
+  },
+  {
+    "revision": "9728da1e09267c549b3f993135600e10",
+    "url": "/memory-game/static/media/dog.9728da1e.jpg"
+  },
+  {
+    "revision": "ace98348c3219bc4cc1332a220f47a35",
+    "url": "/memory-game/static/media/fox.ace98348.jpg"
+  },
+  {
+    "revision": "845468f8ecfe371b9f11fa31b90580e7",
+    "url": "/memory-game/static/media/giraffe.845468f8.jpg"
+  },
+  {
+    "revision": "05235a0b714194f9d76b8f2a05065e38",
+    "url": "/memory-game/static/media/horse.05235a0b.jpg"
+  },
+  {
+    "revision": "206920f8770a98145d63068f91cd8618",
+    "url": "/memory-game/static/media/parrot.206920f8.jpg"
+  },
+  {
+    "revision": "3bc8299da4314913e4b5faa46ff2c13f",
+    "url": "/memory-game/static/media/squid.3bc8299d.jpg"
+  },
+  {
+    "revision": "a25b4d53d680a833668ed530b97d0d35",
+    "url": "/memory-game/static/media/squirrel.a25b4d53.jpg"
+  },
+  {
+    "revision": "d1eb0bd1c112c0ad87409ccac520ae63",
+    "url": "/memory-game/static/media/tiger.d1eb0bd1.jpg"
+  },
+  {
+    "revision": "bc5b5876bcc02dbec57023661473b214",
+    "url": "/memory-game/static/media/duck.bc5b5876.jpg"
+  },
+  {
+    "revision": "806641488ef74357929b5e66a1fd5144",
+    "url": "/memory-game/index.html"
+  }
+];
